b8d2d5a6e37cde92f0b199bad907477451ddbf7d
Bjoern Gruening <bjoern.gruening@gmail.com>
2015-02-12 00:25:16 +0100

    cleanups

--- fetch_and_index_genomes.py
+++ fetch_and_index_genomes.py
@@ -4,15 +4,11 @@ import time
 import argparse
 import urlparse
 import ConfigParser
+import logging as log
 from bioblend.galaxy import GalaxyInstance
 from bioblend.galaxy.client import ConnectionError
 
-if __name__ == '__main__':
-    print "booting up..."
-    parser = argparse.ArgumentParser(description='Fetching and Indexing genomes.')
-    parser.add_argument("--config", required=True, help="Path to config file.")
-
-    options = parser.parse_args()
+def main( options ):
 
     # read from config
     config = ConfigParser.ConfigParser()
@@ -29,73 +25,93 @@ if __name__ == '__main__':
     index_tools = config.get('build_indexers', 'ids').strip().split('\n')
 
     # should test for valid config options
-    
     # establish connection to galaxy instance
     gi = GalaxyInstance(url=url, email=admin_email, password=admin_pass)
 
     # should test valid connection
-    print gi.histories.get_histories()
+    log.info("List of valid histories: %s" % gi.histories.get_histories())
 
     # fetch genomes
-    dbkeys = {}
+    dbkeys = dict()
     for dbkey in genomes:
         if dbkey not in dbkeys:
             tool_inputs={'dbkey':dbkey, 'reference_source|reference_source_selector': 'ucsc', 'reference_source|requested_dbkey': dbkey }
-            print "fetching: ", dbkey, "..."
-            
-            try:                         
+            log.info("Fetching dbkey: %s" % dbkey)
+
+            try:
                 dbkeys[ dbkey ] = gi.tools.run_tool(history_id=None, tool_id=fetch_tool, tool_inputs=tool_inputs)
 
             except ConnectionError as inst:
                 if '\"dbkey\": \"An invalid option was selected' in inst.body:
-                    print "Galaxy instance does not recognize genome key: ", dbkey, "..aborting"
-                    raise
-                else:
-                    raise
-
+                    log.error("Galaxy instance does not recognize genome key: %s" % dbkey)
+                raise
         else:
-            "dbkey (%s) was specified more than once, skipping additional specification." % ( dbkey )
+            log.info("The dbkey (%s) was specified more than once, skipping additional specification." % ( dbkey ))
+
+    gi.make_get_request(urlparse.urljoin(url,'api/tool_data/all_fasta/reload')
 
     # start indexers
-    print "waiting to index"
+    log.info("Start building genome indices.")
     indexing_tools = []
     while dbkeys:
         for dbkey, value in dbkeys.items():
-            print dbkey,": ",gi.datasets.show_dataset( value['outputs'][0]['id'] )['state']
             if gi.datasets.show_dataset( value['outputs'][0]['id'] )['state'] in ['ok', 'error']:
-                
+
                 # refresh the tool data tables
-                print gi.make_get_request(urlparse.urljoin(url,'api/tool_data/all_fasta/reload')).text
+                log.info("Refreshing tool-data tables.")
+                log.info( gi.make_get_request(urlparse.urljoin(url,'api/tool_data/all_fasta/reload')).text )
                 time.sleep(5)
-                print gi.make_get_request(urlparse.urljoin(url,'api/tool_data/all_fasta/reload')).text
-                
+                log.info( gi.make_get_request(urlparse.urljoin(url,'api/tool_data/all_fasta/reload')).text )
+                time.sleep(5)
+                log.info( gi.make_get_request(urlparse.urljoin(url,'api/tool_data/all_fasta/reload')).text )
+                time.sleep(5)
                 for tool_id in index_tools:
-                    print "indexing",dbkey, "with", tool_id
-                    
+                    log.info("Indexing %s with %s." % (dbkey, tool_id) )
                     try:
-                        print "dbkey: ",dbkey
+                        log.info("Foo: Indexing %s with %s." % (dbkey, tool_id) )
                         blah = gi.tools.run_tool(history_id=None, tool_id=tool_id, tool_inputs={ 'all_fasta_source':dbkey })
-                        print "===========================\n",blah,"\n==========================="
+                        log.info("Bla: Indexing %s with %s." % (dbkey, tool_id) )
                         indexing_tools.append(blah)
 
                     except ConnectionError as inst:
                         if '\"text\": \"no tool\",' in inst.body:
-                            print "The tool [", tool_id, "] cannot be located. Please check it is installed in your galaxy instance."
+                            log.info("The tool %s cannot be located. Please check it is installed in your galaxy instance." % (tool_id) )
                         else:
                             raise
+
                 del dbkeys[ dbkey ]
         if dbkeys:
             time.sleep(sleep_time)
-            print ".",
+            log.info(".")
 
-    #Wait for indexers to finish
+    # Wait for indexers to finish
     while indexing_tools:
         for i, indexing_tool_value in enumerate( indexing_tools ):
             if gi.datasets.show_dataset( indexing_tool_value['outputs'][0]['id'] )['state'] in ['ok', 'error']:
-                print 'Finished:', indexing_tool_value
+                log.info('Finished %s.' % indexing_tool_value)
                 del indexing_tools[i]
                 break
         if indexing_tools:
             time.sleep(sleep_time)
-                
-    print 'All indexers have been run, please check results.'
+
+    log.info('All indexers have been run, please check results.')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Fetching and Indexing genomes.'
+    )
+    parser.add_argument("-v", "--verbose", help="Increase output verbosity.",
+                    action="store_true")
+    parser.add_argument("--config", required=True, help="Path to config file.")
+
+    #TODO:  Add options to override the admin_user and admin_password + specify 
+    #       files to upload via command line interface.
+
+    args = parser.parse_args()
+    if args.verbose:
+        log.basicConfig(level=log.DEBUG)
+
+    log.info("Fetch and Index genomes ...")
+    main( args )
+
+
